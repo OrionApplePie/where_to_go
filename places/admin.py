431c77b7ed7579a737de181e41c58533f0aa7cb3
Alexander Ten <orionapplepie@gmail.com>
2021-07-01 13:16:43 +1000

    Добавлено превью фото локаций в админке

--- places/admin.py
+++ places/admin.py
@@ -1,10 +1,29 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from places.models import Place, Image
+
+IMAGE_MAX_HEIGHT = 200
 
 
 class ImageInline(admin.TabularInline):
     model = Image
+
+    fields = ("image", "preview_image", "order_num")
+
+    readonly_fields = [
+        "preview_image",
+    ]
+
+    def preview_image(self, obj):
+        return format_html(
+            '<img src="{}" width="{}" height="{}" />',
+            obj.image.url,
+            IMAGE_MAX_HEIGHT * obj.image.width / obj.image.height,
+            IMAGE_MAX_HEIGHT,
+        )
+
+    preview_image.short_description = "Превью"
 
 
 @admin.register(Place)
